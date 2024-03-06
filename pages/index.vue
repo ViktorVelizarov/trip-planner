21c629d97605a5b0ed7266d1c7b545e956bbf8b1
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-03-06 17:05:04 +0100

    create a default layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
-  <MainLayout>
+    
     <div>
       <h1 class="text-red-500">Restaurant Data</h1>
       <Alert></Alert>
     </div>
-  </MainLayout>
+    
   </template>
   
   <script>
-  import MainLayout from '~/layouts/MainLayout.vue';
-   
+   definePageMeta({
+    layout: "default",
+   });
    const url = 'https://api.content.tripadvisor.com/api/v1/location/189387/details?key=67894D21FC294F35BCE617AA12EBAB55&language=en&currency=USD';
    const options = {method: 'GET', headers: {accept: 'application/json'}};
 

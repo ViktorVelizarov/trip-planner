fb62c84f3b97689750038dba6bd0ae2bed546599
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-03-27 19:29:43 +0100

    create an API route that takes a locationID as parameter and returns photos of the given location

---
+++ server/api/GetLocationPhotos/[locationid].js
@@ -0,0 +1,15 @@
+export default defineEventHandler(async (event) => {
+    const { locationid } = event.context.params;
+    const url = `https://api.content.tripadvisor.com/api/v1/location/${locationid}/photos?key=4A5580EDD8C74C2EB25482F91262F807&language=en&currency=USD`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Referer: 'http://example.com'
+      }
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data; 
+  
+});

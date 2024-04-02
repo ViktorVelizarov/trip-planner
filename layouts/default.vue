32fc35cfed0ce98360ead7ec41ff0def0a888b99
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-04-02 11:53:19 +0200

    create feature branch

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <nav class='sticky h-14 inset-x-0 top-0 z-30 w-full border-b  backdrop-blur-lg transition-all'>
         <div class='flex h-14 items-center justify-between border-b'>
           <div  class='flex flex-row gap-5'>
-            <h1 class="text-3xl font-bold font-madimi ml-3"> Vacation Planner</h1>
+            <h1 class="text-3xl font-bold font-madimi ml-4"> Vacation Planner</h1>
         </div>
     </div>   
     </nav>

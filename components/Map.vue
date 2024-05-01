c8644901e1e350515cb7fa658a5579e2735f9b5f
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-05-01 10:59:35 +0200

    add custom input fields at the top of the map

--- components/Map.vue
+++ components/Map.vue
@@ -7,15 +7,35 @@ import mapboxgl from 'mapbox-gl';
 mapboxgl.accessToken = 'pk.eyJ1IjoidmlrdG9ydmVsIiwiYSI6ImNsdmk1aDV5djFjbGMyanFmODNkbG53ZHMifQ.X1lqvNcIbVceNnL6xJAnXA';
 
 export default {
-    mounted() {
+    props: ['modelValue'],
+    mounted() {    
+        const { lng, lat, zoom, bearing, pitch } = this.modelValue;
   const map = new mapboxgl.Map({
     container: this.$refs.mapContainer,
     style: "mapbox://styles/mapbox/streets-v12", // Replace with your preferred map style
-    center: [-71.224518, 42.213995],
-    zoom: 9,
+    center: [lng, lat],
+    bearing,
+    pitch,
+    zoom,
   });
+  const updateLocation = () =>
+  this.$emit('update:modelValue', this.getLocation());
 
+map.on('move', updateLocation);
+map.on('zoom', updateLocation);
+map.on('rotate', updateLocation);
+map.on('pitch', updateLocation);
   this.map = map;
+},
+methods: {
+  getLocation() {
+    return {
+      ...this.map.getCenter(),
+      bearing: this.map.getBearing(),
+      pitch: this.map.getPitch(),
+      zoom: this.map.getZoom(),
+    }
+  }
 },
 };
 </script>
--- pages/map/index.vue
+++ pages/map/index.vue
@@ -1,12 +1,33 @@
 <template>
     <div id="layout">
-      <Map />
+        <div id="sidebar">Longitude: {{ location.lng.toFixed(4) }} | Latitude: {{ location.lat.toFixed(4) }} | Zoom: {{ location.zoom.toFixed(2) }} | <template v-if="location.bearing">
+  Bearing: {{ location.bearing.toFixed(2) }} |
+</template>
+<template v-if="location.pitch">
+  Pitch: {{ location.pitch.toFixed(2) }} |
+</template></div>
+    <Map v-model="location" />
     </div>
   </template>
   
   <script>
- 
-  </script>
+  import '../node_modules/mapbox-gl/dist/mapbox-gl.css';
+
+  export default {
+    components: {
+      Map
+    },
+    data: () => ({
+  location: {
+    lng: -71.224518,
+    lat: 42.213995,
+    bearing: 0,
+    pitch: 0,
+    zoom: 9
+  }
+}),
+  };
+</script>
   
   <style>
     #layout {
@@ -14,4 +35,17 @@
       display: flex;
       position: relative;
     }
+
+    #sidebar {
+  background-color: rgb(35 55 75 / 90%);
+  color: #fff;
+  padding: 6px 12px;
+  font-family: monospace;
+  z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 12px;
+  border-radius: 4px;
+}
   </style>

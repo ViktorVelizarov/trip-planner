c15cdec10d3371b390da86ab3d6287e5ab37eff0
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-06-13 04:00:14 +0200

    remove next-auth to debug a vercel issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [ '@sidebase/nuxt-auth',
+  modules: [ 
   '@nuxtjs/google-fonts',  '@nuxtjs/tailwindcss', 'shadcn-nuxt', 
 ],
 shadcn: {
